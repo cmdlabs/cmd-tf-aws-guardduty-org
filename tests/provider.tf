9014e71238bc6fac75fd385457ebc93af30f044f
Daniel Lau <daniel.lau@cmd.com.au>
2020-08-05 13:35:16 +1000

    update ci

--- tests/provider.tf
+++ tests/provider.tf
@@ -1,6 +1,6 @@
 provider "aws" {
   version                 = ">= 2.65"
-  profile                 = "cmdlabtf-master"
+  profile                 = "cmdlabtf-sandpit"
   region                  = "ap-southeast-2"
   skip_metadata_api_check = true
 }
